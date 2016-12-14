ca1bc7a4af40f9ecd04adc3d2a074c496f8a1cbc
Mark Marsella <mark@givingassistant.org>
2016-12-13 20:37:27 -0800

    day8 part 1 in progress

--- day8/day8.js
+++ day8/day8.js
@@ -10,15 +10,15 @@ rotate column x=A by B shifts all of the pixels in column A (0 is the left colum
 Pixels that would fall off the bottom appear at the top of the column.
 */
 
-const boardWidth = 50;
-const boardHeight = 6;
+const maxWidth = 50;
+const maxHeight = 6;
 
 
 //for constructing the final array of pixels
 var xCoords = [];  // W
 var yCoords = [0,1,2,3,4,5,6];  // H
 
-
+var masterGrid = [];
 
 
 for(var i=0; i < 50; i++){
@@ -32,26 +32,116 @@ var demoInput = 'rect 3x2';
 function makeRect(dims){
   // grab  X and Y coords from dim 
  	var coords = dims.split('rect ')[1].split('x');
-
+ 	var newCoord;
   var x = parseInt(coords[0]);
   var y = parseInt(coords[1]);  // we get this from parsing dim	
   
-
-console.log('x: ', x);
-console.log('y: ', y);
+	console.log('x: ', x);
+	console.log('y: ', y);
   var rect = []; //inserting coords here
+  // var newArr = [];
 
   //insert X values
   for(var i=0; i < y; i++){
   		//insert y values
     for(var j=0; j < x; j++){
-      rect.push([j,i])
+    		console.log('pushing!');
+    		newCoord = [j,i].toString();
+    		// check if the coord is not in the master grid
+    		// if it isn't --> lets add it
+    		if(masterGrid.indexOf(newCoord) === -1){
+		      masterGrid.push(newCoord);    	
+    		}
     }
   }
-
   console.log('RECT NOW', rect);
+  return masterGrid;
+}
+
+/*
+rect 1x2   --------> everytime we add a rect, we overlap lights
+rotate row y=1 by 5  
+rotate row y=0 by 3
+rect 1x2
+rotate column x=30 by 1
+rotate column x=25 by 1
+rotate column x=10 by 1
+rotate row y=1 by 5
+rotate row y=0 by 2
+*/
+
+var cmd = 'rotate column x=30 by 1';
+var cmd2 = 'rect 1x1';
+
+
+
+// rotates a row RIGHT
+
+// loop through the masterGrid 
+// extract all the points that have x = x_cmd
+// increase all fo the Y values by the second CMD
+var cmd3 = 'rotate row y=0 by 3';
+
+function shiftRow(cmd){
+	var y = cmd.split(' ')[2].split('=')[1];
+	var amt = cmd.split(' ')[4];
+
+	var arr;
+
+	// MAKE FOR LOOP INSTEAD
+	
+	//find coord on master grid
+	masterGrid.forEach(function(el){
+		//find any coords along this axis, increment by amt
+		// if amt > maxWidth, then el === (coord.x + amt) % maxWidth
+		arr = el.split(',');  // [0] is x, [1] is y
+		if(arr[1] === y){
+			arr[0] = (arr[0] + amt) % maxWidth;
+		}
+		masterGrid[el] = arr.join(",")
+	})
+
+}
+
+// rotates a column DOWN
+function shiftColumn(cmd){
+	var x = cmd.split(' ')[2].split('=')[1];
+	var amt = cmd.split(' ')[4];
 }
 
 
-makeRect(demoInput);
+// runs throughh all test case
+function part1(cmd){
+
+	//check line for rect/rotate
+	if(cmd.split(' ')[0] === 'rect'){
+		makeRect(cmd)
+	}
+	else if(cmd.split(' ')[1] === 'column'){
+		shiftY(cmd);
+		}
+	else{
+		shiftX(cmd);
+		}
+	} //end part1
+
+	//if rect, makeRect
+	//if rotate, check for ROW or COLUMN
+	// if row, shiftY
+	// if column, shiftX
+
+	//loop through all input
+
+	//check overlap of lights
+
+
+	//Determine what action to make  
+
+	//if rect --> create new rectangle
+
+	// while rotate === TRUE 
+
+
+
+var x = makeRect(demoInput);
 
